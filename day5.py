d77a9ca112b8cf0be76a8cda47f4eb7d48548243
Warren Moore <warren@wamonite.com>
2020-12-05 13:32:39 +0000

    Day 5 part 2.

--- day5.py
+++ day5.py
@@ -24,9 +24,18 @@ def calculate_seat(row, col):
     return row * 8 + col
 
 
+def find_seat(sorted_seat_list):
+    for (seat, expected) in zip(sorted_seat_list, range(sorted_seat_list[0], sorted_seat_list[-1] + 1)):
+        if seat != expected:
+            return expected
+
+
 def run():
-    print(max(map(lambda v: calculate_seat(*v), map(parse_bp, load_file('data/day5_test.txt')))))
-    print(max(map(lambda v: calculate_seat(*v), map(parse_bp, load_file('data/day5.txt')))))
+    print('part1 test', max(map(lambda v: calculate_seat(*v), map(parse_bp, load_file('data/day5_test.txt')))))
+
+    seat_list = sorted(map(lambda v: calculate_seat(*v), map(parse_bp, load_file('data/day5.txt'))))
+    print('part1', seat_list[-1])
+    print('part2', find_seat(seat_list))
 
 
 if __name__ == "__main__":
