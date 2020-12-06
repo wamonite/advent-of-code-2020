2d16350e007ee35663efbe6fe7f578058f1287d9
Warren Moore <warren@wamonite.com>
2020-12-06 07:42:22 +0000

    Day 6 part 2.

--- day6.py
+++ day6.py
@@ -4,24 +4,47 @@
 from aocutil import load_file
 
 
-def parse_answers(lines):
-    group_set = set()
+def parse_answers_1(lines):
+    answer_set = set()
     for line in lines:
         if line:
-            group_set = group_set.union(set(line))
+            answer_set = answer_set | set(line)
 
         else:
-            yield group_set
-            group_set = set()
+            yield len(answer_set)
+            answer_set = set()
 
-    if group_set:
-        yield group_set
+    if answer_set:
+        yield len(answer_set)
+
+
+def parse_answers_2(lines):
+    all_answer_set = set()
+    invalid_answer_set = set()
+    for line in lines:
+        if line:
+            answer_set = set(line) - invalid_answer_set
+            if all_answer_set or invalid_answer_set:
+                all_answer_set &= answer_set
+            else:
+                all_answer_set = answer_set
+            invalid_answer_set |= answer_set - all_answer_set
+
+        else:
+            yield len(all_answer_set)
+            all_answer_set = set()
+            invalid_answer_set = set()
+
+    if all_answer_set:
+        yield len(all_answer_set)
 
 
 def run():
-    print('part1 test', sum(map(len, parse_answers(load_file('data/day6_test.txt')))))
+    print('part1 test', sum(parse_answers_1(load_file('data/day6_test.txt'))))
+    print('part1', sum(parse_answers_1(load_file('data/day6.txt'))))
 
-    print('part1', sum(map(len, parse_answers(load_file('data/day6.txt')))))
+    print('part2 test', sum(parse_answers_2(load_file('data/day6_test.txt'))))
+    print('part2', sum(parse_answers_2(load_file('data/day6.txt'))))
 
 
 if __name__ == "__main__":
