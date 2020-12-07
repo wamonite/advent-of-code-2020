83796ac53f0e7f25e84f35cde4673f673542f1f4
Warren Moore <warren@wamonite.com>
2020-12-07 08:03:44 +0000

    Day 7 initial data.

---
+++ day7.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from aocutil import load_file
+
+
+def run():
+    print('part1 test', load_file('data/day7_test.txt'))
+
+
+if __name__ == "__main__":
+    run()
