7b89acb2859af2bd7b3c7972eac7971df3383159
Warren Moore <warren@wamonite.com>
2020-12-08 09:10:00 +0000

    Day 8 part 2.

--- day08.py
+++ day08.py
@@ -27,6 +27,7 @@ def run_cmds(cmd_list):
     acc = 0
     cmd_set = set()
     ip = 0
+    ip_max = len(cmd_list)
     while True:
         if ip in cmd_set:
             raise InfiniteLoopException(f'ip = {ip}, acc = {acc}')
@@ -41,6 +42,11 @@ def run_cmds(cmd_list):
 
         ip += val if cmd == JMP else 1
 
+        if ip == ip_max:
+            break
+
+    return acc
+
 
 def handle_run(name, cmd_list):
     try:
@@ -49,12 +55,30 @@ def handle_run(name, cmd_list):
         print(name, 'InfiniteLoopException', e)
 
 
+def iterate_runs(name, cmd_list):
+    for idx in range(len(cmd_list)):
+        cmd, val = cmd_list[idx]
+        if cmd in [NOP, JMP]:
+            new_cmd_list = list(cmd_list)
+            new_cmd_list[idx] = (JMP if cmd == NOP else NOP, val)
+
+            try:
+                print(name, run_cmds(new_cmd_list), '@', idx)
+                break
+
+            except InfiniteLoopException:
+                pass
+
+
 def run():
-    test_data = list(parse_cmds(load_file('data/day8_test.txt')))
-    data = list(parse_cmds(load_file('data/day8.txt')))
+    test_data = list(parse_cmds(load_file('data/day08_test.txt')))
+    data = list(parse_cmds(load_file('data/day08.txt')))
 
     handle_run('part1 test', test_data)
     handle_run('part1', data)
+
+    iterate_runs('part2 test', test_data)
+    iterate_runs('part2', data)
 
 
 if __name__ == "__main__":
