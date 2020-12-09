ceefd711f02922ebfef157f0f12304c75314822f
Warren Moore <warren@wamonite.com>
2020-12-09 07:30:02 +0000

    Day 9 data.

---
+++ day9.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from aocutil import load_file
+
+
+def run():
+    test_data = list(map(int, load_file('data/day9_test.txt')))
+    print(test_data)
+
+
+if __name__ == "__main__":
+    run()
