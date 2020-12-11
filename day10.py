989fb8d2cbbabd117bade90bd45c11fe87996d97
Warren Moore <warren@wamonite.com>
2020-12-11 00:14:21 +0000

    Day 10 part 2.

--- day10.py
+++ day10.py
@@ -2,28 +2,40 @@
 # -*- coding: utf-8 -*-
 
 from aocutil import load_file
+from itertools import tee
+from collections import Counter
+from functools import lru_cache
 
 
 def parse_file(file_name):
     return sorted([int(adapter) for adapter in load_file(file_name)])
 
 
-def walk_adapters_1(adapter_list):
-    last_voltage = 0
-    step_count = {}
-    for adapter in adapter_list:
-        step = adapter - last_voltage
-        step_count.setdefault(step, 0)
-        step_count[step] += 1
-        last_voltage = adapter
+# https://stackoverflow.com/questions/5764782/iterate-through-pairs-of-items-in-a-python-list
+def pairwise(iterable):
+    a, b = tee(iterable)
+    next(b, None)
+    return zip(a, b)
 
-    step_count[3] += 1
-    return step_count
+
+def walk_adapters_1(adapter_list):
+    full_list = [0] + adapter_list + [adapter_list[-1] + 3]
+    return Counter([adapter - adapter_last for adapter_last, adapter in pairwise(full_list)])
 
 
 def print_results_1(name, adapter_list):
     results = walk_adapters_1(adapter_list)
     print(name, results, results[1] * results[3])
+
+
+# takes insanely long without caching
+@lru_cache
+def walk_adapters_2(adapter_list, joltage = 0):
+    if joltage == adapter_list[-1]:
+        return 1
+
+    usable_list = [adapter for adapter in adapter_list if joltage < adapter <= joltage + 3]
+    return sum([walk_adapters_2(adapter_list, usable) for usable in usable_list])
 
 
 def run():
@@ -34,6 +46,11 @@ def run():
     print_results_1('part1 test1', test1_data)
     print_results_1('part1 test2', test2_data)
     print_results_1('part1', data)
+
+    # convert to tuple so lru_cache can hash it
+    print('part2 test1', walk_adapters_2(tuple(test1_data)))
+    print('part2 test2', walk_adapters_2(tuple(test2_data)))
+    print('part2', walk_adapters_2(tuple(data)))
 
 
 if __name__ == "__main__":
