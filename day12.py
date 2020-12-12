cf361b8ecff042f9f35014a4d465a5fae65da49f
Warren Moore <warren@wamonite.com>
2020-12-12 13:07:41 +0000

    Day 12 data.

---
+++ day12.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from aocutil import load_file
+
+
+def run():
+    test_data = load_file('data/day12_test.txt')
+
+
+if __name__ == "__main__":
+    run()
