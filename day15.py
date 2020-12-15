7869a0705a113b81f4675738f71c73a192741598
Warren Moore <warren@wamonite.com>
2020-12-15 18:03:54 +0000

    Day 15 part 2.

--- day15.py
+++ day15.py
@@ -5,16 +5,16 @@ from aocutil import timeit
 
 
 TEST_DATA = [
-    ('0,3,6', 436),
-    ('1,3,2', 1),
-    ('2,1,3', 10),
-    ('1,2,3', 27),
-    ('2,3,1', 78),
-    ('3,2,1', 438),
-    ('3,1,2', 1836),
+    ('0,3,6', 436, 175594),
+    ('1,3,2', 1, 2578),
+    ('2,1,3', 10, 3544142),
+    ('1,2,3', 27, 261214),
+    ('2,3,1', 78, 6895259),
+    ('3,2,1', 438, 18),
+    ('3,1,2', 1836, 362),
 ]
 DATA = '0,1,4,13,15,12,16'
-DEFAULT_STEPS = 2020
+STEPS = (2020, 30000000)
 
 
 def parse_data(input):
@@ -23,9 +23,9 @@ def parse_data(input):
 
 @timeit
 def generate_numbers(start_list, steps):
-    number_lookup = {starting_number: (idx + 1, None) for idx, starting_number in enumerate(start_list)}
+    number_lookup = {starting_number: (idx, None) for idx, starting_number in enumerate(start_list)}
     number_spoken = start_list[-1]
-    for step_count in range(len(start_list) + 1, steps + 1):
+    for step_count in range(len(start_list), steps):
         seen_at_1, seen_at_2 = number_lookup[number_spoken]
         number_spoken = 0 if seen_at_2 is None else seen_at_1 - seen_at_2
         number_spoken_last_seen = number_lookup[number_spoken][0] if number_spoken in number_lookup else None
@@ -35,16 +35,17 @@ def generate_numbers(start_list, steps):
     return number_spoken
 
 
-def print_results_1(name, input, steps = DEFAULT_STEPS):
+def print_results(name, input, steps):
     data = parse_data(input)
     result = generate_numbers(data, steps)
     print(f'{name} {result}')
 
 
 def run():
-    for test_input, test_expected in TEST_DATA:
-        print_results_1(f'part1 test {test_expected} =', test_input)
-    print_results_1(f'part1', DATA)
+    for part in range(2):
+        for test_input, *test_expected in TEST_DATA:
+            print_results(f'part{part + 1} test {test_expected[part]} =', test_input, STEPS[part])
+        print_results(f'part{part + 1}', DATA, STEPS[part])
 
 
 if __name__ == "__main__":
