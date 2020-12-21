721a35791d870c7bcec89f5255b1512c0e8d77d8
Warren Moore <warren@wamonite.com>
2020-12-21 20:13:02 +0000

    Add classes to day 20 part 1.

--- day20.py
+++ day20.py
@@ -4,7 +4,125 @@
 from aocutil import load_file
 from itertools import combinations, product
 from collections import Counter
-from math import prod
+from math import prod, sqrt
+import attr
+
+
+@attr.s
+class Tile:
+    num = attr.ib(validator = attr.validators.instance_of(int))
+    line_list = attr.ib(validator = attr.validators.instance_of(list))
+    _border_id_list = attr.ib(init = False)
+
+    @_border_id_list.default
+    def _calculate_all_list_ids(self):
+        tile_line_list = list(self.line_list)
+        border_id_list = [
+            self._calculate_line_ids(tile_line_list),
+            self._calculate_line_ids(self.flip_vertically(tile_line_list)),
+        ]
+        for _ in range(3):
+            tile_line_list = self.rotate_clockwise(tile_line_list)
+            border_id_list.append(self._calculate_line_ids(tile_line_list))
+            border_id_list.append(self._calculate_line_ids(self.flip_vertically(tile_line_list)))
+
+        return border_id_list
+
+    @staticmethod
+    def _calculate_line_id(tile_line):
+        tile_val = 0
+        for v in tile_line:
+            if v == '#':
+                tile_val |= 1
+            tile_val <<= 1
+        return tile_val
+
+    @staticmethod
+    def _calculate_line_ids(tile_line_list):
+        return [
+            Tile._calculate_line_id(tile_line_list[0]),
+            Tile._calculate_line_id(tile_line_list[-1]),
+            Tile._calculate_line_id([v[0] for v in tile_line_list]),
+            Tile._calculate_line_id([v[-1] for v in tile_line_list]),
+        ]
+
+    @staticmethod
+    def flip_vertically(tile_line_list):
+        return tile_line_list[::-1]
+
+    @staticmethod
+    def rotate_clockwise(tile_line_list):
+        """
+        Rotate tile 90 clockwise https://stackoverflow.com/questions/8421337/rotating-a-two-dimensional-array-in-python
+        """
+
+        return list(zip(*tile_line_list[::-1]))
+
+    def check_connections(self, rhs):
+        connection_list = []
+        for idx_l, idx_r in product(range(8), repeat = 2):
+            if self._border_id_list[idx_l][1] == rhs._border_id_list[idx_r][0]:
+                connection_dir = 'above'
+            elif self._border_id_list[idx_l][0] == rhs._border_id_list[idx_r][1]:
+                connection_dir = 'below'
+            elif self._border_id_list[idx_l][3] == rhs._border_id_list[idx_r][2]:
+                connection_dir = 'left'
+            elif self._border_id_list[idx_l][2] == rhs._border_id_list[idx_r][3]:
+                connection_dir = 'right'
+            else:
+                continue
+
+            connection_list.append((idx_l, idx_r, connection_dir))
+
+        return connection_list
+
+    def print(self):
+        if self.num > 0:
+            print(self.num)
+        for tile_line in self.line_list:
+            print(tile_line)
+        print()
+
+
+@attr.s
+class TileGrid:
+    tile_lookup = attr.ib(validator = attr.validators.instance_of(dict))
+
+    _dimension = attr.ib(init = False)
+    _connection_list = attr.ib(init = False)
+    _connection_count = attr.ib(init = False)
+
+    @property
+    def dimension(self):
+        return self._dimension
+
+    @_dimension.default
+    def _calculate_map_dimension(self):
+        return int(sqrt(len(self.tile_lookup.keys())))
+
+    @_connection_list.default
+    def _check_connections(self):
+        return [
+            (tile_num_l, tile_num_r, tile_connections)
+            for tile_num_l, tile_num_r in combinations(self.tile_lookup.keys(), 2)
+            if (tile_connections := self.tile_lookup[tile_num_l].check_connections(self.tile_lookup[tile_num_r]))
+        ]
+
+    @property
+    def connection_count(self):
+        return self._connection_count
+
+    @_connection_count.default
+    def _count_connections(self):
+        tile_connection_count = Counter()
+        for tile_num_l, tile_num_r, *_ in self._connection_list:
+            tile_connection_count.update((tile_num_l,))
+            tile_connection_count.update((tile_num_r,))
+        count_lookup = {}
+        for num in tile_connection_count:
+            count_set = count_lookup.setdefault(tile_connection_count[num], set())
+            count_set.add(num)
+        return count_lookup
 
 
 def parse_input(line_list):
@@ -16,112 +134,26 @@ def parse_input(line_list):
             tile_num = int(line[line.find(' ') + 1: -1])
 
         elif line:
-            tile_list.append([1 if c == '#' else 0 for c in line])
+            tile_list.append(line)
 
         else:
-            tile_lookup[tile_num] = tile_list
+            tile_lookup[tile_num] = Tile(tile_num, tile_list)
             tile_list = []
     if tile_list:
-        tile_lookup[tile_num] = tile_list
+        tile_lookup[tile_num] = Tile(tile_num, tile_list)
 
-    return tile_lookup
-
-
-def print_tile(tile_line_list, tile_num = None):
-    if tile_num is not None:
-        print(tile_num)
-    for tile_line in tile_line_list:
-        print(''.join(['#' if v else '.' for v in tile_line]))
-    print()
+    return TileGrid(tile_lookup)
 
 
-def rotate_tile(tile_line_list):
-    """
-    Rotate tile 90 clockwise https://stackoverflow.com/questions/8421337/rotating-a-two-dimensional-array-in-python
-    """
-
-    return list(zip(*tile_line_list[::-1]))
-
-
-def flip_tile_vertical(tile_line_list):
-    return tile_line_list[::-1]
-
-
-def get_line_id(tile_line):
-    tile_val = 0
-    for v in tile_line:
-        if v:
-            tile_val |= 1
-        tile_val <<= 1
-    return tile_val
-
-
-def get_tile_id(tile_line_list):
-    return [
-        get_line_id(tile_line_list[0]),
-        get_line_id(tile_line_list[-1]),
-        get_line_id([v[0] for v in tile_line_list]),
-        get_line_id([v[-1] for v in tile_line_list]),
-    ]
-
-
-def get_tile_id_lookup(tile_lookup):
-    tile_id_lookup = {}
-    for tile_num, tile_line_list in tile_lookup.items():
-        tile_id_list = [
-            get_tile_id(tile_line_list),
-            get_tile_id(flip_tile_vertical(tile_line_list)),
-        ]
-        for _ in range(3):
-            tile_line_list = rotate_tile(tile_line_list)
-            tile_id_list.append(get_tile_id(tile_line_list))
-            tile_id_list.append(get_tile_id(flip_tile_vertical(tile_line_list)))
-        tile_id_lookup[tile_num] = tile_id_list
-    return tile_id_lookup
-
-
-def match_tiles(tile_id_list_l, tile_id_list_r):
-    return [
-        (idx_l, idx_r, tile_dir)
-        for idx_l, idx_r in product(range(len(tile_id_list_l)), repeat = 2)
-        if (
-            tile_dir :=
-            ('above' if tile_id_list_l[idx_l][1] == tile_id_list_r[idx_r][0] else '') +
-            ('below' if tile_id_list_l[idx_l][0] == tile_id_list_r[idx_r][1] else '') +
-            ('left' if tile_id_list_l[idx_l][3] == tile_id_list_r[idx_r][2] else '') +
-            ('right' if tile_id_list_l[idx_l][2] == tile_id_list_r[idx_r][3] else '')
-        )
-    ]
-
-
-def get_tile_match_list(tile_id_lookup):
-    return [
-        (tile_num_l, tile_num_r, tile_matches)
-        for tile_num_l, tile_num_r in combinations(tile_id_lookup.keys(), 2)
-        if (tile_matches := match_tiles(tile_id_lookup[tile_num_l], tile_id_lookup[tile_num_r]))
-    ]
-
-
-def find_corner_tiles(tile_match_list):
-    tile_count = Counter()
-    for tile_num_l, tile_num_r, *_ in tile_match_list:
-        tile_count.update((tile_num_l,))
-        tile_count.update((tile_num_r,))
-    return [tile_num for tile_num in tile_count if tile_count[tile_num] == 2]
-
-
-def print_results(name, file_name):
+def print_results_1(name, file_name):
     line_list = load_file(file_name)
-    tile_lookup = parse_input(line_list)
-    tile_id_lookup = get_tile_id_lookup(tile_lookup)
-    tile_match_list = get_tile_match_list(tile_id_lookup)
-    tile_corner_list = find_corner_tiles(tile_match_list)
-    print(f'{name} {prod(tile_corner_list)}')
+    tile_grid = parse_input(line_list)
+    print(f'{name} {prod(tile_grid.connection_count[2])}')
 
 
 def run():
-    print_results(f'part1 test 20899048083289 =', 'data/day20_test.txt')
-    print_results(f'part1', 'data/day20.txt')
+    print_results_1(f'part1 test 20899048083289 =', 'data/day20_test.txt')
+    print_results_1(f'part1', 'data/day20.txt')
 
 
 if __name__ == "__main__":
