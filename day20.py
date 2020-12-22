b5dca8e13f7f49e0fade0628c1cfd2422c49c8ea
Warren Moore <warren@wamonite.com>
2020-12-22 15:34:11 +0000

    Day 20 part 2 overkill!!1!

--- day20.py
+++ day20.py
@@ -8,66 +8,60 @@ from math import prod, sqrt
 import attr
 
 
+POS_MAX = 8
+MONSTER_IMAGE = """                  # 
+#    ##    ##    ###
+ #  #  #  #  #  #   """
+
+
 @attr.s
 class Tile:
     num = attr.ib(validator = attr.validators.instance_of(int))
-    line_list = attr.ib(validator = attr.validators.instance_of(list))
-    _border_id_list = attr.ib(init = False)
+    image_lines = attr.ib(validator = attr.validators.instance_of(list))
+    border_value_list = attr.ib(init = False)
 
-    @_border_id_list.default
-    def _calculate_all_list_ids(self):
-        tile_line_list = list(self.line_list)
-        border_id_list = [
-            self._calculate_line_ids(tile_line_list),
-            self._calculate_line_ids(self.flip_vertically(tile_line_list)),
+    def __attrs_post_init__(self):
+        image_lines = list(self.image_lines)
+        self.border_value_list = [
+            self._calculate_border_values(image_lines),
+            self._calculate_border_values(self.flip_vertically(image_lines)),
         ]
         for _ in range(3):
-            tile_line_list = self.rotate_clockwise(tile_line_list)
-            border_id_list.append(self._calculate_line_ids(tile_line_list))
-            border_id_list.append(self._calculate_line_ids(self.flip_vertically(tile_line_list)))
-
-        return border_id_list
+            image_lines = self.rotate_clockwise(image_lines)
+            self.border_value_list.append(self._calculate_border_values(image_lines))
+            self.border_value_list.append(self._calculate_border_values(self.flip_vertically(image_lines)))
 
     @staticmethod
-    def _calculate_line_id(tile_line):
-        tile_val = 0
-        for v in tile_line:
-            if v == '#':
-                tile_val |= 1
-            tile_val <<= 1
-        return tile_val
-
-    @staticmethod
-    def _calculate_line_ids(tile_line_list):
+    def _calculate_border_values(image_lines):
         return [
-            Tile._calculate_line_id(tile_line_list[0]),
-            Tile._calculate_line_id(tile_line_list[-1]),
-            Tile._calculate_line_id([v[0] for v in tile_line_list]),
-            Tile._calculate_line_id([v[-1] for v in tile_line_list]),
+            get_line_as_int(image_lines[0]),
+            get_line_as_int(image_lines[-1]),
+            get_line_as_int([v[0] for v in image_lines]),
+            get_line_as_int([v[-1] for v in image_lines]),
         ]
 
     @staticmethod
-    def flip_vertically(tile_line_list):
-        return tile_line_list[::-1]
+    def flip_vertically(image_lines):
+        return image_lines[::-1]
 
     @staticmethod
-    def rotate_clockwise(tile_line_list):
+    def rotate_clockwise(image_lines):
         """
         Rotate tile 90 clockwise https://stackoverflow.com/questions/8421337/rotating-a-two-dimensional-array-in-python
         """
 
-        return list(zip(*tile_line_list[::-1]))
+        return [''.join(t) for t in zip(*image_lines[::-1])]
 
     def check_connections(self, rhs):
         connection_list = []
-        for idx_l, idx_r in product(range(8), repeat = 2):
-            if self._border_id_list[idx_l][1] == rhs._border_id_list[idx_r][0]:
+        for idx_l, idx_r in product(range(POS_MAX), repeat = 2):
+            if self.border_value_list[idx_l][1] == rhs.border_value_list[idx_r][0]:
                 connection_dir = 'above'
-            elif self._border_id_list[idx_l][0] == rhs._border_id_list[idx_r][1]:
+            elif self.border_value_list[idx_l][0] == rhs.border_value_list[idx_r][1]:
                 connection_dir = 'below'
-            elif self._border_id_list[idx_l][3] == rhs._border_id_list[idx_r][2]:
+            elif self.border_value_list[idx_l][3] == rhs.border_value_list[idx_r][2]:
                 connection_dir = 'left'
-            elif self._border_id_list[idx_l][2] == rhs._border_id_list[idx_r][3]:
+            elif self.border_value_list[idx_l][2] == rhs.border_value_list[idx_r][3]:
                 connection_dir = 'right'
             else:
                 continue
@@ -76,10 +70,22 @@ class Tile:
 
         return connection_list
 
+    def get_tile(self, pos):
+        image_lines = list(self.image_lines)
+        for current_pos in range(0, POS_MAX, 2):
+            if pos == current_pos:
+                return image_lines
+            elif pos == current_pos + 1:
+                return self.flip_vertically(image_lines)
+            image_lines = self.rotate_clockwise(image_lines)
+
+    def get_borderless_tile(self, pos):
+        return [line[1:-1] for line in self.get_tile(pos)][1:-1]
+
     def print(self):
         if self.num > 0:
             print(self.num)
-        for tile_line in self.line_list:
+        for tile_line in self.image_lines:
             print(tile_line)
         print()
 
@@ -92,52 +98,186 @@ class TileGrid:
     _connection_list = attr.ib(init = False)
     _connection_count = attr.ib(init = False)
 
-    @property
-    def dimension(self):
-        return self._dimension
+    def __attrs_post_init__(self):
+        self._dimension = int(sqrt(len(self.tile_lookup.keys())))
 
-    @_dimension.default
-    def _calculate_map_dimension(self):
-        return int(sqrt(len(self.tile_lookup.keys())))
-
-    @_connection_list.default
-    def _check_connections(self):
-        return [
+        self._connection_list = [
             (tile_num_l, tile_num_r, tile_connections)
             for tile_num_l, tile_num_r in combinations(self.tile_lookup.keys(), 2)
             if (tile_connections := self.tile_lookup[tile_num_l].check_connections(self.tile_lookup[tile_num_r]))
         ]
 
-    @property
-    def connection_count(self):
-        return self._connection_count
-
-    @_connection_count.default
-    def _count_connections(self):
         tile_connection_count = Counter()
         for tile_num_l, tile_num_r, *_ in self._connection_list:
             tile_connection_count.update((tile_num_l,))
             tile_connection_count.update((tile_num_r,))
-        count_lookup = {}
+        self._connection_count = {}
         for num in tile_connection_count:
-            count_set = count_lookup.setdefault(tile_connection_count[num], set())
+            count_set = self._connection_count.setdefault(tile_connection_count[num], set())
             count_set.add(num)
-        return count_lookup
 
-    def _check_map_pos(self, map_list, used_tiles, x, y):
-        return map_list
+    @property
+    def dimension(self):
+        return self._dimension
 
-    def get_map(self):
-        used_tiles = set(self.tile_lookup.keys())
+    @property
+    def connection_count(self):
+        return self._connection_count
+
+    def _check_tile(self, tile_l, check_dir, tile_r):
+        for conn_l_num, conn_r_num, conn_list in self._connection_list:
+            if conn_l_num == tile_l[0] and conn_r_num == tile_r[0]:
+                for conn_p_l, conn_p_r, conn_dir in conn_list:
+                    if tile_l[1] == conn_p_l and tile_r[1] == conn_p_r and check_dir == conn_dir:
+                        return True
+
+        return False
+
+    def _check_map_pos(self, map_list, free_tiles, x, y):
+        if (x == 0 or x == self.dimension - 1) and (y == 0 or y == self.dimension - 1):
+            coord_connections = 2
+        elif x == 0 or x == self.dimension - 1 or y == 0 or y == self.dimension - 1:
+            coord_connections = 3
+        else:
+            coord_connections = 4
+
+        for selected_num in [num for num in self.connection_count[coord_connections] if num in free_tiles]:
+            new_free_tiles = set(free_tiles)
+            new_free_tiles.remove(selected_num)
+            for pos in range(POS_MAX):
+                tile_above = None if y == 0 else map_list[x + (y - 1) * self.dimension]
+                tile_left = None if x == 0 else map_list[(x - 1) + y * self.dimension]
+                tile_test = (selected_num, pos)
+                try:
+                    if tile_above:
+                        if not (
+                            self._check_tile(tile_above, 'above', tile_test) or
+                            self._check_tile(tile_test, 'below', tile_above)
+                        ):
+                            raise ValueError(f'tile above not found {x} {y}')
+
+                    if tile_left:
+                        if not (
+                            self._check_tile(tile_left, 'left', tile_test) or
+                            self._check_tile(tile_test, 'right', tile_left)
+                        ):
+                            raise ValueError(f'tile left not found {x} {y}')
+
+                    new_map = list(map_list)
+                    new_map[x + y * self.dimension] = tile_test
+                    new_x = 0 if x == self.dimension - 1 else x + 1
+                    new_y = y + 1 if x == self.dimension - 1 else y
+
+                    if new_y == self.dimension:
+                        return new_map
+
+                    return self._check_map_pos(new_map, new_free_tiles, new_x, new_y)
+
+                except ValueError:
+                    pass
+
+        raise ValueError(f'no valid selection for {x}, {y}')
+
+    def get_map_solutions(self):
+        map_solutions = []
         for corner_num in self.connection_count[2]:
-            used_tiles.remove(corner_num)
-            for pos in range(8):
+            free_tiles = set(self.tile_lookup.keys())
+            free_tiles.remove(corner_num)
+            for pos in range(POS_MAX):
                 try:
                     map_list = [None] * self.dimension * self.dimension
                     map_list[0] = (corner_num, pos)
-                    return self._check_map_pos(map_list, set(used_tiles), 1, 0)
+                    map_solutions.append(self._check_map_pos(map_list, free_tiles, 1, 0))
+
                 except ValueError:
                     pass
+
+        return map_solutions
+
+    def print_map(self, map_list):
+        line_tile_info_list = []
+        for idx, map_tile_info in enumerate(map_list):
+            x = idx % self.dimension
+            line_tile_info_list.append(map_tile_info)
+            if x == self.dimension - 1:
+                line_tiles_image_lines = []
+                for tile_info in line_tile_info_list:
+                    tile_num, tile_pos = tile_info
+                    tile = self.tile_lookup[tile_num]
+                    line_tiles_image_lines.append(tile.get_tile(tile_pos))
+
+                    print(f'{tile.num:<5}{tile_pos:<6}', end = '')
+                print()
+
+                for y in range(len(line_tiles_image_lines[0])):
+                    print(' '.join([
+                        f'{line_tile_image_lines[y]}'
+                        for line_tile_image_lines in line_tiles_image_lines
+                    ]))
+                print()
+
+                line_tile_info_list = []
+
+    def build_image(self, map_list):
+        map_image = []
+        line_tile_info_list = []
+        for idx, map_tile_info in enumerate(map_list):
+            x = idx % self.dimension
+            line_tile_info_list.append(map_tile_info)
+            if x == self.dimension - 1:
+                line_tiles_image_lines = []
+                for tile_info in line_tile_info_list:
+                    tile_num, tile_pos = tile_info
+                    tile = self.tile_lookup[tile_num]
+                    line_tiles_image_lines.append(tile.get_borderless_tile(tile_pos))
+
+                for y in range(len(line_tiles_image_lines[0])):
+                    map_image.append(''.join([
+                        f'{line_tile_image_lines[y]}'
+                        for line_tile_image_lines in line_tiles_image_lines
+                    ]))
+
+                line_tile_info_list = []
+
+        return map_image
+
+
+def get_line_as_int(line):
+    line_val = 0
+    for c in line:
+        if c == '#':
+            line_val |= 1
+        line_val <<= 1
+    return line_val
+
+
+def get_pixel_count(line):
+    return sum([1 if c == '#' else 0 for c in line])
+
+
+def find_monster(map_image):
+    mon_image_lines = MONSTER_IMAGE.splitlines()
+    mon_mask = [get_line_as_int(line) for line in mon_image_lines]
+    map_width = len(map_image[0])
+    map_height = len(map_image)
+    mon_width = len(mon_image_lines[0])
+    mon_height = len(mon_image_lines)
+    found_list = []
+    for y in range(map_height - mon_height):
+        for x in range(map_width - mon_width):
+            found = True
+            for mon_y in range(mon_height):
+                map_image_line = map_image[y + mon_y][x:x + mon_width]
+                l_val = get_line_as_int(map_image_line)
+                m_val = mon_mask[mon_y]
+                if m_val & l_val != m_val:
+                    found = False
+                    break
+
+            if found:
+                found_list.append((x, y))
+
+    return found_list
 
 
 def parse_input(line_list):
@@ -154,6 +294,7 @@ def parse_input(line_list):
         else:
             tile_lookup[tile_num] = Tile(tile_num, tile_list)
             tile_list = []
+
     if tile_list:
         tile_lookup[tile_num] = Tile(tile_num, tile_list)
 
@@ -169,7 +310,18 @@ def print_results_1(name, file_name):
 def print_results_2(name, file_name):
     line_list = load_file(file_name)
     tile_grid = parse_input(line_list)
-    result = tile_grid.get_map()
+    map_solutions = tile_grid.get_map_solutions()
+    result = 0
+    for map_solution in map_solutions:
+        map_image = tile_grid.build_image(map_solution)
+        found_list = find_monster(map_image)
+        if found_list:
+            found_count = len(found_list)
+            pixel_count = get_pixel_count(''.join(map_image))
+            monster_pixel_count = get_pixel_count(MONSTER_IMAGE) * found_count
+            result = pixel_count - monster_pixel_count
+            break
+
     print(f'{name} {result}')
 
 
